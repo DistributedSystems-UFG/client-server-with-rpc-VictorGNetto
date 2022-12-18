8a6329df5217114fbb25d1671e976ce89f256437
VictorGNetto <victornetto@discente.ufg.br>
2022-12-18 16:08:55 -0300

    Servidor/Cliente RPyC para manipulação de listas

--- client.py
+++ client.py
@@ -1,9 +1,41 @@
 import rpyc
-from constRPYC import * #-
+from constRPYC import *  # -
+
 
 class Client:
-  conn = rpyc.connect(SERVER, PORT) # Connect to the server
-  print (conn.root.exposed_value())
-  conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
-  print (conn.root.exposed_value())   # Print the result
+    conn = rpyc.connect(SERVER, PORT)  # Connect to the server
+
+    print("exposed_value\n", conn.root.exposed_value(), "\n")
+
+    conn.root.exposed_append(6)
+    conn.root.exposed_append(5)
+    conn.root.exposed_append(6)
+    print("exposed_append\n", conn.root.exposed_value(), "\n")
+
+    copy = conn.root.exposed_copy()
+    print("exposed_copy\n", copy, "\n")
+
+    print("exposed_count\n", conn.root.exposed_count(6), "\n")
+
+    conn.root.exposed_extend([7, 7, 8, 9, 9, 9])
+    print("exposed_extend\n", conn.root.exposed_value(), "\n")
+
+    print("exposed_index\n", conn.root.exposed_index(8), "\n")
+
+    conn.root.exposed_insert(1, 5)
+    print("exposed_insert\n", conn.root.exposed_value(), "\n")
+
+    conn.root.exposed_pop(1)
+    print("exposed_pop\n", conn.root.exposed_value(), "\n")
+
+    conn.root.exposed_remove(8)
+    print("exposed_remove\n", conn.root.exposed_value(), "\n")
+
+    conn.root.exposed_reverse()
+    print("exposed_reverse\n", conn.root.exposed_value(), "\n")
+
+    conn.root.exposed_sort()
+    print("exposed_sort\n", conn.root.exposed_value(), "\n")
+
+    conn.root.exposed_clear()
+    print("exposed_clear\n", conn.root.exposed_value(), "\n")
--- server.py
+++ server.py
@@ -1,18 +1,72 @@
 import rpyc
-from constRPYC import * #-
+from constRPYC import *  # -
 from rpyc.utils.server import ThreadedServer
 
+# Expose the following list methods
+#
+# Method    | Description
+# ----------+------------
+# append()  | Adds an element at the end of the list
+# clear()   | Removes all the elements from the list
+# copy()    | Returns a copy of the list
+# count()   | Returns the number of elements with the specified value
+# extend()  | Add the elements of a list (or any iterable), to the end of the current list
+# index()   | Returns the index of the first element with the specified value
+# insert()  | Adds an element at the specified position
+# pop()     | Removes the element at the specified position
+# remove()  | Removes the first item with the specified value
+# reverse() | Reverses the order of the list
+# sort()    | Sorts the list
+
+
 class DBList(rpyc.Service):
-  value = []
+    value = []
 
-  def exposed_append(self, data):
-    self.value = self.value + [data]
-    return self.value
+    def exposed_value(self):
+        return self.value
 
-  def exposed_value(self):
-    return self.value
+    def exposed_append(self, data):
+        self.value = self.value + [data]
+        return self.value
+
+    def exposed_clear(self):
+        self.value.clear()
+        return self.value
+
+    def exposed_copy(self):
+        return self.value.copy()
+
+    def exposed_count(self, n):
+        return self.value.count(n)
+
+    def exposed_extend(self, data):
+        self.value.extend(data)
+        return self.value
+
+    def exposed_index(self, n):
+        return self.value.index(n)
+
+    def exposed_insert(self, pos, data):
+        self.value.insert(pos, data)
+        return self.value
+
+    def exposed_pop(self, pos):
+        self.value.pop(pos)
+        return self.value
+
+    def exposed_remove(self, n):
+        self.value.remove(n)
+        return self.value
+
+    def exposed_reverse(self):
+        self.value.reverse()
+        return self.value
+
+    def exposed_sort(self):
+        self.value.sort()
+        return self.value
+
 
 if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
-  server.start()
-
+    server = ThreadedServer(DBList(), port=PORT)
+    server.start()
